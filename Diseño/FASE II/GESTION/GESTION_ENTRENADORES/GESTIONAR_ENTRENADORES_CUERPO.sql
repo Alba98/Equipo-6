2ba4820262445d19149f972e3ff28880b07d045a
Alba <alba.alonso@ikasle.egibide.org>
2022-05-03 22:44:07 +0200

    Arreglado gestion entrenadores + testing

--- Diseño/FASE II/GESTION/GESTION_ENTRENADORES/GESTIONAR_ENTRENADORES_CUERPO.sql
+++ Diseño/FASE II/GESTION/GESTION_ENTRENADORES/GESTIONAR_ENTRENADORES_CUERPO.sql
@@ -17,33 +17,32 @@ FUNCTION  BUSCAR_PERSONA_POR_NICKNAME
     (P_NICKNAME VARCHAR2)
     RETURN NUMBER
     AS
-        P_NUM_PERSONA PERSONA.NICKNAME%TYPE;
+        P_COD_PERSONA PERSONA.COD_PERSONA%TYPE;
     BEGIN
         -- Comprobar que existe la persona
-        SELECT COD_PERSONA INTO P_NUM_PERSONA
+        SELECT COD_PERSONA INTO P_COD_PERSONA
         FROM PERSONA
         WHERE UPPER(NICKNAME) = UPPER(P_NICKNAME);
         -- devolver el codigo de la persona
-        RETURN P_NUM_PERSONA;
+        RETURN P_COD_PERSONA;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
     -- lleno P_NUM_PERSONA con null para que mi funcion indique que no existe el
     -- nickname en la base de datos
-        P_NUM_PERSONA:=NULL;
-         RAISE_APPLICATION_ERROR (-20001 ,'Err. nickname no encontrado');
-        RETURN P_NUM_PERSONA;
+        P_COD_PERSONA:=NULL;
+        RETURN P_COD_PERSONA;
         
     WHEN TOO_MANY_ROWS THEN
     -- lleno P_NUM_PERSONA con -1 para que mi funcion indique que existe mas de 
     -- una pesrona con el mismo nickname
-        P_NUM_PERSONA:=-1;
-        RETURN P_NUM_PERSONA;
+        P_COD_PERSONA:=-1;
+        RETURN P_COD_PERSONA;
 END BUSCAR_PERSONA_POR_NICKNAME;
 
 -- **************************************************************
 /*
-Procedimiento INSERT_ENTRENADOR: insertar un ENTRENADOR tras haber sido insertados 
-sus datos en personas
+Procedimiento INSERT_ENTRENADOR: insertar un entrenador tras haber sido 
+insertados sus datos en personas
 */
 PROCEDURE INSERT_ENTRENADOR(
 	P_NICKNAME PERSONA.NICKNAME%TYPE)
@@ -73,7 +72,7 @@ END INSERT_ENTRENADOR;
 
 -- **************************************************************
 /*
-Procedimiento INSERT_JUGADOR: insertar un ENTRENADOR insertando los datos en 
+Procedimiento INSERT_JUGADOR: insertar un entrenador insertando los datos en 
 persona
 */
 PROCEDURE INSERT_PERSONA(
@@ -85,20 +84,20 @@ PROCEDURE INSERT_PERSONA(
     P_SUELDO PERSONA.SUELDO%TYPE
     )
 AS
-    V_COD_JUGADOR NUMBER;
+    V_COD_ENTRENADOR NUMBER;
     DUP_VAL_ON_INDEX EXCEPTION;
 BEGIN
 	-- comprobando que no existe otra persona con ese nickname
-    V_COD_JUGADOR := BUSCAR_PERSONA_POR_NICKNAME(P_NICKNAME);
+    V_COD_ENTRENADOR := BUSCAR_PERSONA_POR_NICKNAME(P_NICKNAME);
     
-    IF (V_COD_JUGADOR IS NULL)
+    IF (V_COD_ENTRENADOR IS NULL)
     THEN      
         --insertar persona
         INSERT INTO PERSONA (NICKNAME, NOMBRE, APELLIDO, FECHA_NACIMIENTO, 
             NACIONALIDAD, SUELDO)
         VALUES (UPPER(P_NICKNAME), UPPER(P_NOMBRE), UPPER(P_APELLIDO), 
             P_FECHA_NACIMIENTO, UPPER(P_NACIONALIDAD), P_SUELDO);
-        -- insertar jugador
+        -- insertar entrenador
         INSERT_ENTRENADOR(P_NICKNAME);
     ELSE
         RAISE DUP_VAL_ON_INDEX;
