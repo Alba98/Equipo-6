228b86d4ddd181cc60a856945359993958328463
Alba <alba.alonso@ikasle.egibide.org>
2022-05-03 19:43:29 +0200

    Arreglado gestion jugadores + testing

--- Diseño/FASE II/GESTION/GESTION_JUGADORES/GEST_JUGADORES_CUERPO.sql
+++ Diseño/FASE II/GESTION/GESTION_JUGADORES/GEST_JUGADORES_CUERPO.sql
@@ -17,27 +17,26 @@ FUNCTION  BUSCAR_PERSONA_POR_NICKNAME
     (P_NICKNAME VARCHAR2)
     RETURN NUMBER
     AS
-        P_NUM_PERSONA PERSONA.NICKNAME%TYPE;
+        P_COD_PERSONA PERSONA.COD_PERSONA%TYPE;
     BEGIN
         -- Comprobar que existe la persona
-        SELECT COD_PERSONA INTO P_NUM_PERSONA
+        SELECT COD_PERSONA INTO P_COD_PERSONA
         FROM PERSONA
         WHERE UPPER(NICKNAME) = UPPER(P_NICKNAME);
         -- devolver el codigo de la persona
-        RETURN P_NUM_PERSONA;
+        RETURN P_COD_PERSONA;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
     -- lleno P_NUM_PERSONA con null para que mi funcion indique que no existe el
     -- nickname en la base de datos
-        P_NUM_PERSONA:=NULL;
-         RAISE_APPLICATION_ERROR (-20001 ,'Err. nickname no encontrado');
-        RETURN P_NUM_PERSONA;
+        P_COD_PERSONA:=NULL;
+        RETURN P_COD_PERSONA;
         
     WHEN TOO_MANY_ROWS THEN
     -- lleno P_NUM_PERSONA con -1 para que mi funcion indique que existe mas de 
     -- una pesrona con el mismo nickname
-        P_NUM_PERSONA:=-1;
-        RETURN P_NUM_PERSONA;
+        P_COD_PERSONA:=-1;
+        RETURN P_COD_PERSONA;
 END BUSCAR_PERSONA_POR_NICKNAME;
 
 -- **************************************************************
@@ -78,14 +77,14 @@ Procedimiento INSERT_JUGADOR: insertar un jugador insertando los datos en
 persona
 */
 PROCEDURE INSERT_PERSONA(
-    P_NICKNAME PERSONA.NICKNAME%TYPE,
-    P_ROL JUGADORES.ROL%TYPE,
-	P_NOMBRE PERSONA.NOMBRE%TYPE,
-	P_APELLIDO PERSONA.APELLIDO%TYPE,
-	P_FECHA_NACIMIENTO PERSONA.FECHA_NACIMIENTO%TYPE,
-	P_NACIONALIDAD PERSONA.NACIONALIDAD%TYPE,
-    P_SUELDO PERSONA.SUELDO%TYPE
-    )
+        P_NICKNAME PERSONA.NICKNAME%TYPE,
+        P_ROL JUGADORES.ROL%TYPE,
+        P_NOMBRE PERSONA.NOMBRE%TYPE,
+        P_APELLIDO PERSONA.APELLIDO%TYPE,
+        P_FECHA_NACIMIENTO PERSONA.FECHA_NACIMIENTO%TYPE,
+        P_NACIONALIDAD PERSONA.NACIONALIDAD%TYPE,
+        P_SUELDO PERSONA.SUELDO%TYPE
+        )
 AS
     V_COD_JUGADOR NUMBER;
     DUP_VAL_ON_INDEX EXCEPTION;
@@ -93,7 +92,7 @@ BEGIN
 	-- comprobando que no existe otra persona con ese nickname
     V_COD_JUGADOR := BUSCAR_PERSONA_POR_NICKNAME(P_NICKNAME);
     
-    IF (V_COD_JUGADOR IS not NULL)
+    IF (V_COD_JUGADOR IS NULL)
     THEN      
         --insertar persona
         INSERT INTO PERSONA (NICKNAME, NOMBRE, APELLIDO, FECHA_NACIMIENTO, 
@@ -101,10 +100,11 @@ BEGIN
         VALUES (UPPER(P_NICKNAME), UPPER(P_NOMBRE), UPPER(P_APELLIDO), 
             P_FECHA_NACIMIENTO, UPPER(P_NACIONALIDAD), P_SUELDO);
         -- insertar jugador
-        INSERT_JUGADOR(P_NICKNAME, P_ROL);
+       INSERT_JUGADOR(P_NICKNAME, P_ROL);
     ELSE
         RAISE DUP_VAL_ON_INDEX;
-    END IF;       
+    END IF;
+    
 EXCEPTION
     WHEN DUP_VAL_ON_INDEX THEN
         RAISE_APPLICATION_ERROR (-20003 ,'Err. nickname departamento duplicado');
