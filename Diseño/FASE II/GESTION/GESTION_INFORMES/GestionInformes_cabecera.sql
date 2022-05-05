78c0f63c811307e5cb92aad6f9c1518781e19817
Ania <aniasura.khiari@ikasle.egibide.org>
2022-05-05 13:25:12 +0200

    GESTIONAR_INFORMES

---
+++ Diseño/FASE II/GESTION/GESTION_INFORMES/GestionInformes_cabecera.sql
@@ -0,0 +1,41 @@
+/*********Cabecera**************/
+
+
+CREATE OR REPLACE PACKAGE GESTION_INFORMES AS 
+TYPE TCURSOR IS REF CURSOR;
+
+   PROCEDURE p_resultados(p_num_jornada IN OUT NUMBER,
+                           p_cod_equipo OUT NUMBER,
+                           p_resultado OUT VARCHAR2,
+                           p_horapartido OUT varchar2);
+    
+   PROCEDURE obtener_datos_jugadores(p_nick_jugador IN OUT  varchar2,
+                                      p_nom_equipo OUT varchar2,
+                                      p_apellido_jugador OUT varchar2,
+                                      p_nom_jugador OUT varchar2,
+                                      p_rol_jugador OUT varchar2);
+                                           
+    PROCEDURE obtener_datos_equipos(c_nombre IN VARCHAR2,
+                                    c_equipo OUT tcursor);
+                                    
+                                           
+    
+   PROCEDURE obtener_datos_entrenador (p_nom_equipo OUT varchar2,
+                                        p_nombre OUT varchar2,
+                                        p_apellido  OUT varchar2,
+                                        p_nick IN OUT varchar2,
+                                        p_nacionalidad   OUT varchar2);
+            
+     END  GESTION_INFORMES;                           
+                                        
+    
+
+   
+
+
+
+
+
+
+
+
