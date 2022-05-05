16385dd1596e7648733307a8ab517a1b58594fb8
Alba <alba.alonso@ikasle.egibide.org>
2022-05-05 12:00:29 +0200

    INSERT_PARTIDO en proceso

--- Diseño/FASE II/GESTION/GESTION_PARTIDOS/GESTIONAR_PARTIDOS_CUERPO.sql
+++ Diseño/FASE II/GESTION/GESTION_PARTIDOS/GESTIONAR_PARTIDOS_CUERPO.sql
@@ -6,37 +6,38 @@
  
 CREATE OR REPLACE PACKAGE BODY GEST_PARTIDOS AS
 
-FUNCTION  BUSCAR_PARTIDO_POR_NOMBRE
+FUNCTION  BUSCAR_EQUIPO_POR_NOMBRE
     (P_NOMBRE VARCHAR2)
     RETURN NUMBER;
-      
+          
 -- **************************************************************
 -- Programacion de la funcion
-FUNCTION  BUSCAR_PARTIDO_POR_NOMBRE
+FUNCTION  BUSCAR_EQUIPO_POR_NOMBRE
     (P_NOMBRE VARCHAR2)
     RETURN NUMBER
     AS
-        P_COD_PARTIDO EQUIPOS.COD_EQUIPO%TYPE;
+        P_COD_EQUIPO EQUIPOS.COD_EQUIPO%TYPE;
     BEGIN
-        -- Comprobar que existe la persona
-        SELECT COD_EQUIPO INTO P_COD_PARTIDO
+        -- Comprobar que existe el equipo
+        SELECT COD_EQUIPO INTO P_COD_EQUIPO
         FROM EQUIPOS
         WHERE UPPER(NOMBRE) = UPPER(P_NOMBRE);
-        -- devolver el codigo de la persona
-        RETURN P_COD_PARTIDO;
+        -- devolver el codigo del equipo
+        RETURN P_COD_EQUIPO;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-    -- lleno P_NUM_PERSONA con null para que mi funcion indique que no existe el
-    -- nickname en la base de datos
-        P_COD_PARTIDO:=NULL;
-        RETURN P_COD_PARTIDO;
+    -- lleno P_COD_EQUIPO con null para que mi funcion indique que no existe el
+    -- nombre de equipo en la base de datos
+        P_COD_EQUIPO:=NULL;
+        RETURN P_COD_EQUIPO;
         
     WHEN TOO_MANY_ROWS THEN
-    -- lleno P_NUM_PERSONA con -1 para que mi funcion indique que existe mas de 
-    -- una pesrona con el mismo nickname
-        P_COD_PARTIDO:=-1;
-        RETURN P_COD_PARTIDO;
-END BUSCAR_PARTIDO_POR_NOMBRE;
+    -- lleno P_COD_EQUIPO con -1 para que mi funcion indique que existe mas de 
+    -- una equipo con el mismo nombre
+        P_COD_EQUIPO:=-1;
+        RETURN P_COD_EQUIPO;
+END BUSCAR_EQUIPO_POR_NOMBRE;
+
 
 -- **************************************************************
 /*
@@ -49,36 +50,50 @@ PROCEDURE INSERT_PARTIDO(
         P_EQUIPO2 EQUIPOS.NOMBRE%TYPE
         )
 AS
-    V_COD_TEMPORADA NUMBER;
     V_COD_EQUIPO1 NUMBER;
     V_COD_EQUIPO2 NUMBER;
+    V_COD_PARTIDO NUMBER;
+    EQUIPO1_NO_ENCONTRADO EXCEPTION;
+    EQUIPO2_NO_ENCONTRADO EXCEPTION;
 BEGIN
 	-- comprobando que no existe otra equipo con ese nombre
-    V_COD_EQUIPO1 := BUSCAR_PARTIDO_POR_NOMBRE(P_EQUIPO1);
-    V_COD_EQUIPO2 := BUSCAR_PARTIDO_POR_NOMBRE(P_EQUIPO2);
-    /*
-    IF (V_COD_TEMPORADA IS NULL)
-    THEN 
-        RAISE TEMPORADA_NO_EXISTE;
-    ELSIF (V_COD_TEMPORADA = -1)
-    THEN
-        RAISE DUP_VAL_ON_INDEX;
-    ELSE
-       --insertar jornada
-        INSERT INTO JORNADAS(COD_TEMPORADA, FECHA_JORNADA)
-        VALUES (P_COD_TEMPORADA, P_FECHA_JORNADA);
-    END IF;
-    */
-    NULL;
+    V_COD_EQUIPO1 := BUSCAR_EQUIPO_POR_NOMBRE(P_EQUIPO1);
+    V_COD_EQUIPO2 := BUSCAR_EQUIPO_POR_NOMBRE(P_EQUIPO2);
+    
+    IF (V_COD_EQUIPO1 IS NULL OR V_COD_EQUIPO2 IS NULL)
+    THEN      
+        RAISE EQUIPO1_NO_ENCONTRADO;
+    ELSE 
+        IF (V_COD_EQUIPO2 IS NULL)
+        THEN      
+            RAISE EQUIPO2_NO_ENCONTRADO;
+        ELSE 
+        NULL;
+             --insertar el partido
+            INSERT INTO PARTIDOS (HORA_PARTIDO, COD_JORNADA) 
+            VALUES (P_HORA_PARTIDO, P_COD_JORNADA);
+           /*
+            --obtener codigo partido
+            SELECT COD_PARTIDO INTO V_COD_PARTIDO
+            FROM PARTIDOS
+            WHERE COD_JORNADA = P_COD_JORNADA
+              AND HORA_PARTIDO = P_HORA_PARTIDO;
+            
+            --insertar participa
+            INSERT INTO PARTICIPA (COD_EQUIPO1, COD_EQUIPO2, COD_PARTIDO) 
+            VALUES (V_COD_EQUIPO1, V_COD_EQUIPO2, V_COD_PARTIDO);
+        */
+        END IF;   
+    END IF; 
 EXCEPTION
-	WHEN OTHERS THEN
-        RAISE_APPLICATION_ERROR('-20998','Error desconocido');
+    WHEN EQUIPO1_NO_ENCONTRADO THEN
+        RAISE_APPLICATION_ERROR (-20001 ,'Err. el equipo1' || P_EQUIPO1 ||'no existe');
+	 WHEN EQUIPO2_NO_ENCONTRADO THEN
+        RAISE_APPLICATION_ERROR (-20002 ,'Err. el equipo2' || P_EQUIPO2 ||'no existe');
+    WHEN OTHERS THEN
+        RAISE_APPLICATION_ERROR('-20999','Error desconocido INSERT_PARTIDO');
 END INSERT_PARTIDO;
 
--- **************************************************************
-/*
-Procedimiento INSERT_RESULTADO: insertar resultado de un partido
-*/
 PROCEDURE INSERT_RESULTADO(
     P_COD_PARTIDO PARTIDOS.COD_PARTIDO%TYPE,
     P_RESULTADO PARTIDOS.RESULTADO%TYPE
