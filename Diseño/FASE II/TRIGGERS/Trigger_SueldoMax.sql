0dfd0d6644e76e3ca0e6bb307e7e2291adcbad77
Victor Ibañez Emborujo <victor.ibanez@ikasle.egibide.org>
2022-05-06 10:38:17 +0200

    TriggerSueldo

--- Diseño/FASE II/TRIGGERS/Trigger_SueldoMax.sql
+++ Diseño/FASE II/TRIGGERS/Trigger_SueldoMax.sql
@@ -1,22 +1,33 @@
 CREATE OR REPLACE TRIGGER T_SueldoMax
-  BEFORE INSERT on personas
+  BEFORE INSERT on jugar_para
     FOR EACH ROW
       DECLARE
         v_codEquipo NUMBER;
         v_sueldosTotales NUMBER;
+        v_codJugadores jugar_para.cod_jugador%type;
         v_SueldoMaxTotal NUMBER:=200000;
         v_error EXCEPTION;
+        CURSOR c_jugadores IS(
+            SELECT cod_jugador
+            FROM jugar_para
+            WHERE cod_equipo = :NEW.cod_equipo
+            );
     BEGIN
-        SELECT SUM(p.sueldo)
-          INTO v_sueldosTotales
-        FROM personas p, jugadores j, jugar_para jp
-        WHERE p.cod_persona = j.cod_jugador
-        AND j.cod_jugador = jp.cod_jugador
-        AND jp.cod_equipo = v_codEquipo;
+        OPEN c_jugadores;
+        LOOP
+            FETCH c_jugadores INTO v_codJugadores;
+            EXIT WHEN c_jugadores%NOTFOUND;
+        END LOOP;
+        CLOSE c_jugadores;
         
-        v_sueldosTotales := v_sueldosTotales + v_sueldosTotales;
-        
+        LOOP
+            SELECT SUM(p.sueldo)INTO v_sueldosTotales
+            FROM personas p
+            WHERE p.cod_persona = v_codJugadores;
+            
+            v_sueldosTotales := v_sueldosTotales + v_sueldosTotales;
+        END LOOP;
         IF(v_sueldosTotales> v_SueldoMaxTotal) THEN
-          RAISE_APPLICATION_ERROR(-20250,'Estás introduciendo un salario inferior al salario mínimo');         
+          RAISE_APPLICATION_ERROR(-20250,'Estas introduciendo un salario inferior al salario minimo');         
       END IF;
 END;
