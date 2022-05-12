544045a5e953bf7648d9d1ff979a68216f9f1d87
Ania <aniasura.khiari@ikasle.egibide.org>
2022-05-12 22:55:28 +0200

    Ventana Login y Registro

---
+++ Consultoria/src/main/java/Vista/VLogin.java
@@ -0,0 +1,12 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VLogin {
+    private JPanel pPrincipal;
+    private JTextField tFCorreo;
+    private JTextField textField2;
+    private JButton loginButton;
+    private JLabel tFContraseña;
+}
+
---
+++ Consultoria/src/main/java/Vista/VRegistrar.java
@@ -0,0 +1,12 @@
+package Vista;
+
+import javax.swing.*;
+
+public class VRegistrar {
+    private JPanel pPrincipal;
+    private JTextField tfNombre;
+    private JTextField tfCorreo;
+    private JTextField tfFechaNacimiento;
+    private JTextField tfcontraseña;
+    private JButton registrarButton;
+}
