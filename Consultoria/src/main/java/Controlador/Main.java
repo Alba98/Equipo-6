d17cb51dff4576c9f99ecf7678c63814a130e411
Ania <aniasura.khiari@ikasle.egibide.org>
2022-05-13 12:43:49 +0200

    CambiosVista

--- Consultoria/src/main/java/Controlador/Main.java
+++ Consultoria/src/main/java/Controlador/Main.java
@@ -1,28 +1,35 @@
 package Controlador;
 import Vista.*;
-
 import javax.swing.*;
 
-public class Main {
-    static JFrame frame;
+        public class Main {
+        static JFrame frame;
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         try {
             System.out.println("CONSULTORIA E-SPORTS ");
-                VLogin();
-        } catch (Exception e) {
-            System.out.println("Problemas con la base de datos " + e.getMessage());
+            VLogin();
         }
+            catch (Exception e) {
+            System.out.println("Problemas con la base de datos " + e.getMessage());
+         }
 
-        public static void VLogin(){
-            frame=new JFrame("Inicio de sesion");
+         }
+
+         public static void VLogin() {
+
+            frame = new JFrame("Inicio de sesion");
             frame.setContentPane(new VLogin().pPrincipal);
             frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
 
-        }
+            }
+
+
+
+
 
     }
-}
+
