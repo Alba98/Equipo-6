bba9b7c27cb9bdb63ff689d039fe422263eed37c
Ania <aniasura.khiari@ikasle.egibide.org>
2022-05-16 12:22:10 +0200

    Excepciones

--- Consultoria/src/main/java/Controlador/Main.java
+++ Consultoria/src/main/java/Controlador/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            bd=new BaseDatos();
+            pDAO=new PersonasDAO(bd.getConnection());
+
             System.out.println("CONSULTORIA E-SPORTS ");
         } catch (Exception e) {
             System.out.println("Problemas con la base de datos " + e.getMessage());
