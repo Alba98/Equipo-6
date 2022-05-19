850fbd087ee3036df338b90f5840cbbb109279aa
Ania <aniasura.khiari@ikasle.egibide.org>
2022-05-19 10:58:09 +0200

    XMLclasificacion

--- FASE IV(20-05-22)/GenerarXMLClasificacion.sql
+++ FASE IV(20-05-22)/GenerarXMLClasificacion.sql
@@ -1,27 +1,25 @@
-CREATE TABLE temp_clob_tab(
-    result CLOB
-);
 
-DECLARE
+--
+CREATE TABLE temp_clob_tab(result CLOB);
+--                       
+    DECLARE
     qryCTX DBMS_XMLGEN.ctxHandle;
     result CLOB;
-BEGIN
-	qryCTX := DBMS_XMLGEN.newContext('SELECT * FROM VISTA_CLASIFICACION');
-									 
-DBMS_XMLGEN.setRowSetTag(qryCtx,'clasificacion');
+    BEGIN
+    qryCTX := DBMS_XMLGEN.newContext('SELECT * FROM VISTA_CLASIFICACION');
+                           
+    DBMS_XMLGEN.setRowSetTag(qryCtx,'clasificacion');
 
-DBMS_XMLGEN.setRowTag(qryCtx,'temporada');
+    DBMS_XMLGEN.setRowTag(qryCtx,'equipo');
 
-result:= DBMS_XMLGEN.getXML(qryCtx);
+    result:= DBMS_XMLGEN.getXML(qryCtx);
+    INSERT INTO temp_clob_tab VALUES(result);
 
-INSERT INTO temp_clob_tab VALUES(result);
+    DBMS_XMLGEN.closeContext(qryCtx);
 
-DBMS_XMLGEN.closeContext(qryCtx);
-
-end;
-
+    end;
+--
 SELECT * FROM temp_clob_tab;
 
-DELETE FROM temp_clob_tab;
 
 
