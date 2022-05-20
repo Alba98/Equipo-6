c7da794182dc8923c206f639bbf4f3f936dbd1eb
Victor Ibañez Emborujo <victor.ibanez@ikasle.egibide.org>
2022-05-20 13:47:12 +0200

    Trigger_SueldoMax y Test  UPDATE

--- FASE II (06-05-2022)/TRIGGERS/BLOQUE-ANONIMO_TEST/Trigger_SueldoMax_test.sql
+++ FASE II (06-05-2022)/TRIGGERS/BLOQUE-ANONIMO_TEST/Trigger_SueldoMax_test.sql
@@ -15,5 +15,8 @@ set sueldo = 2500;
 
 end;
 
+INSERT INTO personas (nombre, apellido, sueldo, fecha_nacimiento, nacionalidad, nickname) 
+VALUES ('Darcy','Wuenz',200001,TO_DATE('14/05/2001','DD/MM/YYYY'),'Taiwanesa','The_Core');
+
 
 rollback;
